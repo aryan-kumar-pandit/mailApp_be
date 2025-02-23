0478f0cc30b6b06f076b0690c2e18f3fe1e257f2
aryan-kumar-pandit <aryankumarpandito@gmail.com>
2025-02-23 10:44:46 +0530

    project started

---
+++ src/main/java/com/send/mail/MailAppBeApplication.java
@@ -0,0 +1,14 @@
+package com.send.mail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailAppBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MailAppBeApplication.class, args);
+
+	}
+
+}
